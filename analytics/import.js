fbc729b1e5828e6beaa71bc4ac7fcbb8ee48825e
script-js <134544514+script-js@users.noreply.github.com>
2024-03-18 20:14:52 -0700

    Update import.js

--- analytics/import.js
+++ analytics/import.js
@@ -1,11 +1,6 @@
 function analyticsInit(id) {
-  if (document.title) {
-    var title = document.title
-  } else {
-    var title = location.href
-  }
   var anframe = document.createElement("iframe")
   anframe.style = "display:none";
-  anframe.src = "https://script-js.github.io/analytics/log?metaURL=" + location.href + "&metaID=" + id;
+  anframe.src = "https://script-js.github.io/analytics/log?metaID=" + id;
   document.documentElement.appendChild(anframe)
 }
